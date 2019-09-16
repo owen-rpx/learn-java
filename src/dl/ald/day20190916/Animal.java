c2e0455cfc198d9ce275e0a701788c5a157cc4ac
test <xudongz@rocketsoftware.com>
2019-09-16 12:39:56 +0800

    extends

--- src/dl/ald/day20190916/Animal.java
+++ src/dl/ald/day20190916/Animal.java
@@ -1,6 +1,9 @@
 package dl.ald.day20190916;
 
 public class Animal {
+	public Animal(){
+		System.out.println("动物类执行了");
+	}
 	@SuppressWarnings("unused")
 	private int age;
 	public String name;
--- src/dl/ald/day20190916/Dog.java
+++ src/dl/ald/day20190916/Dog.java
@@ -1,11 +1,14 @@
 package dl.ald.day20190916;
 
 public class Dog extends Animal {
+	public Dog() {
+		System.out.println("狗类执行了");
+	}
 
 	@Override
 	public void eat() {
 		// TODO Auto-generated method stub
-//		super.eat();
+		// super.eat();
 		System.out.println("狗具有吃的能力");
 	}
 
